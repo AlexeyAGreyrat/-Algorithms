eb5b25af07227a8b9aed4eda132cd305167d5125
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2021-02-10 17:18:44 +0300

    Add files via upload

---
+++ Lab1/Vertex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Lab1
+{
+    class Vertex
+    {
+        public int Number { get; set; }
+        public Vertex(int number)
+        {
+            Number = number;
+        }
+    }
+}
